67dbea67c6066e77c228ba33739faa3cf45ced79
ClaytonChisholm <59948532+ClaytonChisholm@users.noreply.github.com>
2022-01-31 20:13:12 -0500

    Add files via upload

--- simulation.py
+++ simulation.py
@@ -4,8 +4,13 @@ import time
 
 print('works')
 physicsClient = p.connect(p.GUI)
+
+p.loadSDF("box.sdf")
+
 for i in range(1000):
     print(i)
     p.stepSimulation()
     time.sleep(1/60)
+
+
 p.disconnect()
