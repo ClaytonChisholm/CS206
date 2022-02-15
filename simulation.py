a2e238b2de03a56042c196d6a23782ff56b8b87b
ClaytonChisholm <59948532+ClaytonChisholm@users.noreply.github.com>
2022-02-14 20:47:53 -0500

    Add files via upload
    initial commit

--- simulation.py
+++ simulation.py
@@ -1,11 +1,43 @@
 import pybullet as p
 import time
+import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy
+
 
 
 print('works')
 physicsClient = p.connect(p.GUI)
-for i in range(1000):
-    print(i)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+
+#gravity
+p.setGravity(0,0,-9.8)
+
+#add floor
+planeId = p.loadURDF("plane.urdf")
+
+#load box
+p.loadSDF("world.sdf")
+
+#robot id
+robotId = p.loadURDF("body.urdf")
+
+#prepare
+pyrosim.Prepare_To_Simulate(robotId)
+
+backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
+
+#for loop
+for i in range(0, 1000):
     p.stepSimulation()
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(1/60)
+#save data
+numpy.save("data/dataBackLeg.npy", backLegSensorValues, allow_pickle=True, fix_imports=True)
+numpy.save("data/datafrontLeg.npy", frontLegSensorValues, allow_pickle=True, fix_imports=True)
+print(backLegSensorValues)
+
 p.disconnect()
